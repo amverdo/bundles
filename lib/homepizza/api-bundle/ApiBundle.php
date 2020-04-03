b33bdbb052beb6872a419fb297c280b7504f8886
Alexandr Malozemov <is.malozemov@ya.ru>
2020-04-03 20:58:24 +0500

    Part of config apibundle

---
+++ lib/homepizza/api-bundle/ApiBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Homepizza\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ApiBundle extends Bundle
+{
+
+}
---
+++ lib/homepizza/api-bundle/src/DependencyInjection/HomepizzaApiExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Homepizza\ApiBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class HomepizzaApiExtension extends Extension
+{
+    /**
+     * Инициализация контейнера, назначение аргументов
+     *
+     * @param array $configs
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        // TODO: Implement load() method.
+    }
+}

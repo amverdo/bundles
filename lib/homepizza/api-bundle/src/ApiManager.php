a5943415d3e720e5f9412dece0234d067d4b4ee1
Alexandr Malozemov <is.malozemov@ya.ru>
2020-04-07 17:48:45 +0500

    Structure of methods

--- lib/homepizza/api-bundle/src/ApiManager.php
+++ lib/homepizza/api-bundle/src/ApiManager.php
@@ -11,50 +11,65 @@ use Homepizza\ApiBundle\DTO\Responses\BonusesResponse;
 use Homepizza\ApiBundle\DTO\Responses\CustomerResponse;
 use Homepizza\ApiBundle\DTO\Responses\OrderResponse;
 use Homepizza\ApiBundle\DTO\Responses\TimeResponse;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Symfony\Component\Cache\Adapter\AdapterInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface AS HttpClient;
+use Symfony\Component\Cache\Adapter\AdapterInterface AS Adapter;
+use Symfony\Component\DependencyInjection\ContainerInterface AS Container;
+use Homepizza\ApiBundle\Service\ApiTransformer;
 
 class ApiManager implements ApiManagerInterface
 {
-    /* @var HttpClientInterface */
+    /* @var HttpClient */
     private $http;
 
-    /* @var AdapterInterface */
+    /* @var Adapter */
     private $cache;
 
     /* @var string */
     private $uri;
 
-    public function __construct(HttpClientInterface $http, AdapterInterface $cache, ContainerInterface $container)
+    /* @var ApiTransformer */
+    private $transformer;
+
+    public function __construct(HttpClient $http, Adapter $cache, ApiTransformer $transformer, Container $container)
     {
         $this->http = $http;
         $this->cache = $cache;
         $this->uri = $container->getParameter('homepizza.api_link');
+        $this->transformer = $transformer;
     }
 
     public function customerProfile(string $phone): CustomerResponse
     {
         // TODO: Implement customerProfile() method.
+
+        return $this->transformer->transformResponse(new CustomerResponse(), []);
     }
 
     public function customerAddresses(string $phone): array
     {
         // TODO: Implement customerAddresses() method.
+
+        return $this->transformer->transformResponse(new AddressResponse(), []);
     }
 
     public function customerBonuses(string $phone): BonusesResponse
     {
         // TODO: Implement customerBonuses() method.
+
+        return $this->transformer->transformResponse(new BonusesResponse(), []);
     }
 
     public function checkTime(Delivery $delivery, Order $order): TimeResponse
     {
         // TODO: Implement checkTime() method.
+
+        return $this->transformer->transformResponse(new TimeResponse(), []);
     }
 
     public function createOrder(Customer $customer, Delivery $delivery, Order $order): OrderResponse
     {
         // TODO: Implement createOrder() method.
+
+        return $this->transformer->transformResponse(new OrderResponse(), []);
     }
 }
--- lib/homepizza/api-bundle/src/Service/ApiTransformer.php
+++ lib/homepizza/api-bundle/src/Service/ApiTransformer.php
@@ -2,10 +2,92 @@
 
 namespace Homepizza\ApiBundle\Service;
 
+use Homepizza\ApiBundle\DTO\Responses\AddressResponse;
+use Homepizza\ApiBundle\DTO\Responses\BonusesResponse;
+use Homepizza\ApiBundle\DTO\Responses\CustomerResponse;
+use Homepizza\ApiBundle\DTO\Responses\OrderResponse;
+use Homepizza\ApiBundle\DTO\Responses\TimeResponse;
+
 /**
  * Трансформирование ответов в объекты и объектов в запросы (с проверкой)
  */
 class ApiTransformer
 {
+    /**
+     * Наполнение объекта
+     *
+     * @param $object
+     * @param array $data
+     * @return mixed
+     */
+    public function transformResponse($object, array $data)
+    {
+        if ($object instanceof CustomerResponse) $object = $this->toProfile($object, $data);
+        if ($object instanceof AddressResponse) $object = $this->toAddresses($object, $data);
+        if ($object instanceof BonusesResponse) $object = $this->toBonuses($object, $data);
+        if ($object instanceof TimeResponse) $object = $this->toTime($object, $data);
+        if ($object instanceof OrderResponse) $object = $this->toOrder($object, $data);
 
+        return $object;
+    }
+
+    /**
+     * Данные клиента
+     *
+     * @param CustomerResponse $object
+     * @param array $data
+     * @return CustomerResponse
+     */
+    private function toProfile(CustomerResponse $object, array $data)
+    {
+        return $object;
+    }
+
+    /**
+     * Адреса клиента
+     *
+     * @param AddressResponse $object
+     * @param array $data
+     * @return array
+     */
+    private function toAddresses(AddressResponse $object, array $data)
+    {
+        return [];
+    }
+
+    /**
+     * Бонусный баланс клиента
+     *
+     * @param BonusesResponse $object
+     * @param array $data
+     * @return BonusesResponse
+     */
+    private function toBonuses(BonusesResponse $object, array $data)
+    {
+        return $object;
+    }
+
+    /**
+     * Доступное время
+     *
+     * @param TimeResponse $object
+     * @param array $data
+     * @return TimeResponse
+     */
+    private function toTime(TimeResponse $object, array $data)
+    {
+        return $object;
+    }
+
+    /**
+     * Результат создания заказа
+     *
+     * @param OrderResponse $object
+     * @param array $data
+     * @return OrderResponse
+     */
+    private function toOrder(OrderResponse $object, array $data)
+    {
+        return $object;
+    }
 }
